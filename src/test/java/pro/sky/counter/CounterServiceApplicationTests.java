9538cbec55cea1e3f78e5bccd3181bfae853e792
Айкелен <Aikelen.030904@gmail.com>
2025-05-14 20:50:53 +0300

    commit

---
+++ src/test/java/pro/sky/counter/CounterServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package pro.sky.counter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CounterServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
