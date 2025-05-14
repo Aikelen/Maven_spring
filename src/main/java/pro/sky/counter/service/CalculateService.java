cdb32a6a047a55f3f7cb230876928a8a1d86327a
Айкелен <Aikelen.030904@gmail.com>
2025-05-14 20:57:14 +0300

    commi

--- src/main/java/pro/sky/counter/service/CalculateService.java
+++
@@ -1,8 +0,0 @@
-package pro.sky.counter.service;
-
-public interface CalculateService {
-    int add(int a, int b);
-    int subtract(int a, int b);
-    int multiply(int a, int b);
-    double divide(int a, int b);
-}
